e9694fef2619dd959e8befae66e26fdd85896d5b
Jorge Ramirez <joramirez96@gmail.com>
2020-03-28 19:11:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,10 @@
 # Write your code here.
-def badge_maker ("Jorge")
+def badge_maker(speaker)
+  puts "Hello, my name is #{speaker}"
+end 
+
+def batch_badge_creator(array)
+  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  puts "Hello, my name is #{array}"
 
 end 
