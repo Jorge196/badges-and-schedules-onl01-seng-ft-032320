81c16c0ee07235ffb4dc31672ab9cab375fdb65b
Jorge Ramirez <joramirez96@gmail.com>
2020-03-28 19:52:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,9 @@ def badge_maker(name)
 
 end
 
-  speakers_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(speakers_array)
+
+def batch_badge_creator(array) 
   new_array = []
     array.each do |name|
       new_array.push ("Hello, my name is #{name}.")
